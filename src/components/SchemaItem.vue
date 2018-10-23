eebe8d9282e473a11ce920d21434a380b2ec943f
MurWhite <oomofeioo@qq.com>
2018-10-23 22:26:26 +0800

    保存

--- src/components/SchemaItem.vue
+++ src/components/SchemaItem.vue
@@ -1,3 +1,19 @@
 <template>
-    
+    <div class="schema-item-wrap">
+        <div class="schema-key">
+            <input type="text" >
+        </div>
+        <div class="schema-value"></div>
+    </div>
 </template>
+<script>
+export default {
+  data() {},
+  props: {
+    key: String,
+    value: String | Number,
+    readonly: Boolean
+  },
+  methods: {}
+};
+</script>
